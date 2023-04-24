40b7674d1d81847805ffd05881c74f47522b7953
LEE <dheosdh1458@naver.com>
2023-04-24 10:50:53 +0900

    1

--- TestForm/DB.cs
+++ TestForm/DB.cs
@@ -21,21 +21,21 @@ namespace TestForm
             try
             {
                 List<SalesPrice> salesPrice_list = new List<SalesPrice>();
-                using (SqlConnection conn = new SqlConnection("server = 182.162.90.215; uid = unospay; pwd =dnshtmvpdl0223!; database =unospay;"))
-                {
-                    string sql = "select * from Sales_price with(nolock) where affiliate_id = 'test_demo2' and  CONVERT(CHAR(8), timestamp, 112) = '20221102'";
+                //using (SqlConnection conn = new SqlConnection("server = 182.162.90.215; uid = unospay; pwd =dnshtmvpdl0223!; database =unospay;"))
+                //{
+                //    string sql = "select * from Sales_price with(nolock) where affiliate_id = 'test_demo2' and  CONVERT(CHAR(8), timestamp, 112) = '20221102'";
 
-                    conn.Open();
-                    SqlCommand cmd = new SqlCommand(sql, conn);
-                    SqlDataReader rdr = cmd.ExecuteReader();
-                    while (rdr.Read())
-                    {
-                        SalesPrice salesPrice = new SalesPrice();
-                        salesPrice.pay_type = Convert.ToString(rdr["pay_type"]);
-                        salesPrice.pay_price = Convert.ToString(rdr["pay_price"]);
-                        salesPrice_list.Add(salesPrice);
-                    }
-                }
+                //    conn.Open();
+                //    SqlCommand cmd = new SqlCommand(sql, conn);
+                //    SqlDataReader rdr = cmd.ExecuteReader();
+                //    while (rdr.Read())
+                //    {
+                //        SalesPrice salesPrice = new SalesPrice();
+                //        salesPrice.pay_type = Convert.ToString(rdr["pay_type"]);
+                //        salesPrice.pay_price = Convert.ToString(rdr["pay_price"]);
+                //        salesPrice_list.Add(salesPrice);
+                //    }
+                //}
                 return salesPrice_list;
             }
             catch (Exception e)
